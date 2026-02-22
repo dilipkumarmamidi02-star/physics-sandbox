8acc6ece483312a3d92459321283ae69261a1fd2
dilipkumarmamidi02-star <dilipkumarmamidi02@gmail.com>
2026-02-22 13:10:10 +0530

    Fix Supabase lock timeout

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -3,4 +3,11 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://jkirrwlfmuvucysuxkid.supabase.co'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpraXJyd2xmbXV2dWN5c3V4a2lkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE2OTQxNzIsImV4cCI6MjA4NzI3MDE3Mn0.ghqEoEJZ8OP9uoJ9KiEDIyXxYxieY9PZ9KhE3q4--PY'
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    persistSession: true,
+    storageKey: 'phx-auth',
+    storage: window.localStorage,
+    lockAcquireTimeout: 30000
+  }
+})
